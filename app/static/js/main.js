c5935c7d6cbc9b080ae6cf96a716139e32048d37
Emerson Suarez <emersonsuarez2904@gmail.com>
2025-05-17 13:46:45 -0500

    feat: :sparkles: agrega sections hero calendar

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,25 @@
-// Scripts JS si los necesitas
+document.addEventListener("DOMContentLoaded", function () {
+	const calendarEl = document.getElementById("calendar");
+	const calendar = new FullCalendar.Calendar(calendarEl, {
+		initialView: "dayGridMonth",
+		locale: "es",
+		headerToolbar: {
+			left: "prev",
+			center: "title",
+			right: "next",
+		},
+		fixedWeekCount: false,
+		events: [
+			{
+				title: "Visita regular",
+				start: "2025-05-21T16:30:00",
+			},
+			{
+				title: "Cumpleaños abuela Isabel",
+				start: "2025-05-23",
+				allDay: true,
+			},
+		],
+	});
+	calendar.render();
+});
